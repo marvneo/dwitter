1b43146a6f66a6ad54dd509d768ee84d64091fc5
Marv Neoh <neoh@ualberta.ca>
2022-01-26 12:15:58 -0800

    created dweets, tracks user and date, can handle 140 characters

--- dwitter/admin.py
+++ dwitter/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group, User
-from .models import Profile
+from .models import Profile, Dweet
 
 # Register your models here.
 class ProfileInline(admin.StackedInline):
@@ -15,3 +15,4 @@ class UserAdmin(admin.ModelAdmin):
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
 admin.site.unregister(Group)
+admin.site.register(Dweet)
--- dwitter/models.py
+++ dwitter/models.py
@@ -5,8 +5,6 @@ from django.dispatch import receiver
 
 # Create your models here.
 
-
-
 class Profile(models.Model):
 
     #represents the profile's connection to a User entitity
@@ -31,3 +29,18 @@ def create_profile(sender, instance, created, **kwargs):
         user_profile.follows.set([instance.profile.id])
         user_profile.save()
 
+
+
+class Dweet(models.Model):
+    user = models.ForeignKey(
+        User, related_name="dweets", on_delete=models.DO_NOTHING
+    )
+    body = models.CharField(max_length=140)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return(
+            f"{self.user}"
+            f"({self.created_at:%Y-%m-%d %H:%M}): "
+            f"{self.body[:30]}..."
+        )
